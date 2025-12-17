690fe616c2bbeca36bc19a8f377566bfed1919a8
kai-stein <kai_holstein@hotmail.co.uk>
2025-12-17 10:19:38 +0000

    adding api call and envirmonet variables

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
     print("Hello from gemini-ai!")
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if not api_key:
+        raise RuntimeError("No api key in .env file")
+    
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model='gemini-2.5-flash', contents='Why is the sky blue?')
+    print(response.text)
 
 
 if __name__ == "__main__":
