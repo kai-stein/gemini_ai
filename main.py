199cb926b4bf2a80f031da98c2debca0309043e9
kai-stein <kai_holstein@hotmail.co.uk>
2025-12-27 13:16:56 +0000

    use functions directed by model responce

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 import argparse
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions,call_function
 
 def main():
     print("Hello from gemini-ai!")
@@ -46,6 +46,13 @@ def main():
 
     for function_call in response.function_calls:
         print(f"Calling function: {function_call.name}({function_call.args})")
+        function_call_result = call_function(function_call,args.verbose)
+        if function_call_result.parts[0].function_response is None:
+            raise Exception("No function responce")
+        if function_call_result.parts[0].function_response.response is None:
+            raise Exception("No function_response.response")
+        if args.verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 
         
 if __name__ == "__main__":
